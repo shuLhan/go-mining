91eb9717a87a62d240effd86caecf02007c88325
Mhd Sulhan <ms@kilabit.info>
2015-10-12 02:56:15 +0700

    distance: Rename Record to Sample, less ambigous from dsv.Record.

--- knn/distance.go
+++ knn/distance.go
@@ -11,7 +11,7 @@ import (
 Distance is a mapping between record and their distance.
 */
 type Distance struct {
-	Record	*dsv.RecordSlice
+	Sample	*dsv.RecordSlice
 	Value	float64
 }
 
@@ -25,7 +25,7 @@ NewDistance create new distance object.
 */
 func NewDistance () *Distance {
 	return &Distance {
-		Record	: nil,
+		Sample	: nil,
 		Value	: 0.0,
 	}
 }
@@ -34,7 +34,7 @@ func NewDistance () *Distance {
 String format the distance object.
 */
 func (d Distance) String () (s string) {
-	s += fmt.Sprint (d.Record)
+	s += fmt.Sprint (d.Sample)
 	s += " "
 	s += strconv.FormatFloat (d.Value, 'f', -1, 64)
 	s += "\n"
@@ -65,12 +65,12 @@ Swap content of object in index i with index j.
 func (jarak *DistanceSlice) Swap (i, j int) {
 	var tmp = &Distance{};
 
-	tmp.Record = (*jarak)[i].Record
+	tmp.Sample = (*jarak)[i].Sample
 	tmp.Value = (*jarak)[i].Value
 
-	(*jarak)[i].Record = (*jarak)[j].Record
+	(*jarak)[i].Sample = (*jarak)[j].Sample
 	(*jarak)[i].Value = (*jarak)[j].Value
 
-	(*jarak)[j].Record = tmp.Record
+	(*jarak)[j].Sample = tmp.Sample
 	(*jarak)[j].Value = tmp.Value
 }
