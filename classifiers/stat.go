b5fba04eb13f18bdaed009b8ced5da1a0bb5460e
Mhd Sulhan <ms@kilabit.info>
2016-04-06 14:36:08 +0700

    classifiers/stat: add field Id, elapsedtime, tp, fp, tn, fn, and oob error.
    * StartTime, EndTime: replace with unix timestamp value (int64).
    * Sum(): new method to sum value from other statistic.
    * ToRow(): new method to convert statistic to tabula.Row.

--- classifiers/stat.go
+++ classifiers/stat.go
@@ -5,7 +5,7 @@
 package classifiers
 
 import (
-	"time"
+	"github.com/shuLhan/tabula"
 )
 
 /*
@@ -13,10 +13,27 @@ Stat hold statistic value of classifier, including TP rate, FP rate, precision,
 and recall.
 */
 type Stat struct {
-	// startTime contain the start time of classifier.
-	StartTime time.Time
-	// endTime contain the end time of classifier.
-	EndTime time.Time
+	// Id unique id for this statistic (e.g. number of tree).
+	Id int64
+	// StartTime contain the start time of classifier in unix timestamp.
+	StartTime int64
+	// EndTime contain the end time of classifier in unix timestamp.
+	EndTime int64
+	// ElapsedTime contain actual time, in seconds, between end and start
+	// time.
+	ElapsedTime int64
+	// TP contain true-positive value.
+	TP int64
+	// FP contain false-positive value.
+	FP int64
+	// TN contain true-negative value.
+	TN int64
+	// FN contain false-negative value.
+	FN int64
+	// OobError contain out-of-bag error.
+	OobError float64
+	// OobErrorMean contain mean of out-of-bag error.
+	OobErrorMean float64
 	// TPRate contain true-positive rate (recall): tp/(tp+fn)
 	TPRate float64
 	// FPRate contain false-positive rate: fp/(fp+tn)
@@ -36,4 +53,47 @@ Recall return value of recall.
 */
 func (stat *Stat) Recall() float64 {
 	return stat.TPRate
+}
+
+//
+// Sum will add statistic from other stat object to current stat, not including
+// the start and end time.
+//
+func (stat *Stat) Sum(other *Stat) {
+	stat.OobError += other.OobError
+	stat.OobErrorMean += other.OobErrorMean
+	stat.TP += other.TP
+	stat.FP += other.FP
+	stat.TN += other.TN
+	stat.FN += other.FN
+	stat.TPRate += other.TPRate
+	stat.FPRate += other.FPRate
+	stat.Precision += other.Precision
+	stat.FMeasure += other.FMeasure
+	stat.Accuracy += other.Accuracy
+}
+
+//
+// ToRow will convert the stat to tabula.row in the order of Stat field.
+//
+func (stat *Stat) ToRow() (row *tabula.Row) {
+	row = &tabula.Row{}
+
+	row.PushBack(tabula.NewRecordInt(stat.Id))
+	row.PushBack(tabula.NewRecordInt(stat.StartTime))
+	row.PushBack(tabula.NewRecordInt(stat.EndTime))
+	row.PushBack(tabula.NewRecordInt(stat.ElapsedTime))
+	row.PushBack(tabula.NewRecordReal(stat.OobError))
+	row.PushBack(tabula.NewRecordReal(stat.OobErrorMean))
+	row.PushBack(tabula.NewRecordInt(stat.TP))
+	row.PushBack(tabula.NewRecordInt(stat.FP))
+	row.PushBack(tabula.NewRecordInt(stat.TN))
+	row.PushBack(tabula.NewRecordInt(stat.FN))
+	row.PushBack(tabula.NewRecordReal(stat.TPRate))
+	row.PushBack(tabula.NewRecordReal(stat.FPRate))
+	row.PushBack(tabula.NewRecordReal(stat.Precision))
+	row.PushBack(tabula.NewRecordReal(stat.FMeasure))
+	row.PushBack(tabula.NewRecordReal(stat.Accuracy))
+
+	return
 }
